697d4ce53c0fb8bfddb666cd4937d0f428e23426
Kolotilov Artem <144371183+Screo-IS@users.noreply.github.com>
2024-02-10 22:30:31 +0300

    Lab_3

---
+++ Lab_3/Complex.h
@@ -0,0 +1,26 @@
+#ifndef LAB_3_COMPLEX_H
+#define LAB_3_COMPLEX_H
+
+
+class Complex {
+
+public:
+
+    double re;
+    double im;
+
+public:
+
+    Complex();
+    explicit Complex(double re);
+    Complex(double re, double im);
+
+    Complex operator*(Complex& another) const;
+    Complex operator*=(double num);
+    Complex operator+(Complex& another) const;
+    double operator|=(Complex& same) const;
+
+};
+
+
+#endif
