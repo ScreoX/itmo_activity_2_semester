88002fe479e33750ebc0ff0b9486f41aedf75405
Kolotilov Artem <144371183+Screo-IS@users.noreply.github.com>
2024-02-25 21:29:55 +0300

    Add OP Lab_6 wihout tests

---
+++ Lab_6/Functions.h
@@ -0,0 +1,14 @@
+#ifndef LAB_6_FUNCTIONS_H
+#define LAB_6_FUNCTIONS_H
+
+template<typename T>
+bool anyOfDivisibleBy2 (T obj);
+
+template<typename T>
+bool isPartitioned (T obj, T elem);
+
+template<typename T>
+bool IsPalindrome (T obj1, T obj2);
+
+#include "Functions.cpp"
+#endif
