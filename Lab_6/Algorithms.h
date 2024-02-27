849eb440666380abede64a325580acdc50ba05e0
Kolotilov Artem <144371183+Screo-IS@users.noreply.github.com>
2024-02-27 12:05:56 +0300

    Add tests

--- Lab_6/Algorithms.h
+++ Lab_6/Algorithms.h
@@ -11,26 +11,30 @@ bool any_of(const iterator& begin, const iterator& end, bool (&function)(obj)) {
     return false;
 }
 
-template<typename Iterator, typename obj>
-bool is_partitioned(const Iterator& begin, const Iterator& end, bool (&function)(obj)) {
-    for (Iterator elem = begin; elem != end; elem++) {
-        bool check = false;
-        for (Iterator i = begin; i != end && i != elem; i++) {
-            if (function(*i, *elem)) {
-                if (check) {
-                    return true;
+template<typename iterator, typename obj1, typename obj2>
+bool is_partitioned(const iterator& begin, const iterator& end, bool (&function)(obj1, obj2)) {
+    for (iterator elem = begin; elem != end; elem++) {
+        bool check[2] = {false, false};
+        for (iterator i = begin; i != end; i++) {
+            if (i != elem) {
+                if (function(*i, *elem)) {
+                    check[0] = true;
+                } else {
+                    check[1] = true;
                 }
-                check = true;
             }
+        }
+        if (check[0] && check[1]) {
+            return true;
         }
     }
     return false;
 }
 
-template<typename Iterator, typename obj>
-bool is_palindrome(const Iterator& begin, const Iterator& end, bool (&function)(obj)) {
-    for (Iterator i = begin, j = end - 1; i != end, j != begin; i++, j--) {
-        if (function(*i, *j)) {
+template<typename iterator, typename obj1>
+bool is_palindrome(const iterator& begin, const iterator& end, bool (&function)(obj1)) {
+    for (iterator i = begin, j = end - 1; i != end, j != begin; i++, j--) {
+        if (function(*i) ^ function(*j)) {
             return false;
         }
     }
--- Lab_6/main.cpp
+++ Lab_6/main.cpp
@@ -1,14 +1,90 @@
 #include <iostream>
 #include "vector"
 #include "Algorithms.h"
-#include "Functions.h"
+#include "Complex.h"
+
+template<typename T>
+bool anyOfLessThanZero (T obj) {
+    return obj < 0;
+}
+
+template<typename T>
+bool isPartitioned (T obj, T elem) {
+    return obj == elem;
+}
+
+template<typename T>
+bool isPalindrome (T obj1) {
+    return obj1 > 0;
+}
 
 int main() {
-    std::vector<int> s = {1,3,5};
-    if (any_of(s.begin(), s.end(), anyOfDivisibleBy2<int>)) {
+
+    std::cout << "Int Vector:\n";
+    std::vector<int> v_nums = {2,2,5};
+    std::vector<int>::iterator itr_for_int;
+    for (itr_for_int = v_nums.begin(); itr_for_int != v_nums.end(); itr_for_int++) {
+        std::cout << *itr_for_int << " ";
+    }
+
+    std::cout << "\nAny of numbers less than zero:\n";
+    if (any_of(v_nums.begin(), v_nums.end(), anyOfLessThanZero<int>)) {
         std::cout << "YES\n";
     } else {
         std::cout << "NO\n";
     }
+
+    std::cout << "Numbers can be partitioned by predicate (equal):\n";
+    if (is_partitioned(v_nums.begin(), v_nums.end(), isPartitioned<int>)) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+
+    std::cout << "Numbers of vector represent palindrome by predicate(Int num larger than zero):\n";
+    if (is_palindrome(v_nums.begin(), v_nums.end(), isPalindrome<int>)) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+
+
+    std::cout << "\nComplex Vector\n";
+
+    Complex x(1,4);
+    Complex y(1,4);
+    Complex z(-2, -10);
+    std::vector<Complex> v_complex_nums = {x, y, z};
+
+    std::vector<Complex>::iterator itr_for_complex;
+    for (itr_for_complex = v_complex_nums.begin(); itr_for_complex != v_complex_nums.end(); itr_for_complex++) {
+        if (itr_for_complex->im >= 0) {
+            std::cout << itr_for_complex->re << " + " << itr_for_complex->im << "i\n";
+        } else {
+            std::cout << itr_for_complex->re << " - " << itr_for_complex->im * -1 << "i\n";
+        }
+    }
+
+    std::cout << "Any of numbers have less than zero:\n";
+    if (any_of(v_complex_nums.begin(), v_complex_nums.end(), anyOfLessThanZero<Complex>)) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+
+    std::cout << "Numbers can be partitioned by predicate (equal):\n";
+    if (is_partitioned(v_complex_nums.begin(), v_complex_nums.end(), isPartitioned<Complex>)) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+
+    std::cout << "Numbers of vector represent palindrome by predicate(Complex num larger than zero):\n";
+    if (is_palindrome(v_complex_nums.begin(), v_complex_nums.end(), isPalindrome<Complex>)) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+
     return 0;
 }
