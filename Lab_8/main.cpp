c22a67509d6c387b6301679dd653ddd59d554f0b
Kolotilov Artem <144371183+ScreoX@users.noreply.github.com>
2024-04-30 11:26:57 +0300

    Delete Lab_8 directory

--- Lab_8/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "MainCube.h"
-
-int main() {
-
-    MainCube mainScreen; // создаем экран, где будет находиться куб
-    mainScreen.init(); // иницилизируем куб
-    MainCube::changePositions(mainScreen); // меняем расположение кубика с помощью клавиш
-
-}
